1fcdd268bf09d5eb172e404991318c6a6fb4b462
Jude <storeyjude@hotmail.com>
2020-04-20 14:23:10 +0000

    fixed file structure

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,10 +11,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
-    
+
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contacts.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
